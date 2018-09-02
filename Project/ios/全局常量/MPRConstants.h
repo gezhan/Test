d463ab2d8dcaffd01be3bd4f659940e0895dac03
gezhan <gezhan@binghaodeMacBook-Pro.local>
2018-09-02 19:51:41 +0800

    1

---
+++ Project/ios/全局常量/MPRConstants.h
@@ -0,0 +1,25 @@
+//
+//  MPRConstants.h
+//  MPR
+//
+//  Created by huang on 2018/7/4.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPRConstants : NSObject
+
+/** 高德地图--appkey */
+extern NSString *const MPRConstants_GaodeMapAppkey;
+
+/** 友盟统计--appkey */
+extern NSString *const MPRConstants_UmSocialAppkey;
+
+/** 腾讯Bugly */
+extern NSString *const MPRConstants_TencentAppId;
+
+/** 极光推送--appkey */
+extern NSString *const MPRConstants_JPUSHAppAppkey;
+
+@end
---
+++ Project/ios/全局常量/MPRConstants.m
@@ -0,0 +1,26 @@
+//
+//  MPRConstants.m
+//  MPR
+//
+//  Created by huang on 2018/7/4.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import "MPRConstants.h"
+
+@implementation MPRConstants
+
+
+/** 高德地图--appkey */
+NSString *const MPRConstants_GaodeMapAppkey = @"232f6f5ab67dfe743a86cce6b9a3e180";
+
+/** 友盟统计--appkey */
+NSString *const MPRConstants_UmSocialAppkey = @"5b3b5bc3b27b0a73810001ca";
+
+/** 腾讯Bugly */
+NSString *const MPRConstants_TencentAppId = @"310f11bd2f";
+
+/** 极光推送--appkey */
+NSString *const MPRConstants_JPUSHAppAppkey = @"3e24848df7a153e620b95b19";
+
+@end
